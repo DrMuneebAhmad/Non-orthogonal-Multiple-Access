f09ddb96f9cf02aeb7ce062aaba18b1bfab875ad
Muneeb Ahmad <muneeb.ahmad72@gmail.com>
2023-01-11 11:46:29 +0900

    Update NOMA.m
    MATLAB File

--- NOMA.m
+++ NOMA.m
@@ -1,20 +1,15 @@
 % n0.0f bits for transmit signal: 100 bits only - Multiply '/25' for 4 bits
-% as original SMT/TNY codes;
 close all; clear all;clc;
         TxBits_n = 100;
         t_s=0.001:0.001:0.1;
-% distance from Base Station (BS) to Primary User, to User1, to User2 - SMT
-% Assuming maximum distance as 10, for attenuation calculation purposes
+% distance from Base Station (BS) to Primary User, to User1, to User2
+% Assuming d_max=10, attenuation calculation
         DstBStoPUser_dp = 3;
         DstBStoUser1_ds1 = 2;
         DstBStoUser2_ds2 = 4;
         MaxDsttoUser_00 = 10;
       SumSquareDst_00=DstBStoPUser_dp^2+DstBStoUser1_ds1^2+DstBStoUser2_ds2^2;
-% signal from BS - Power allocation already applied on signal(???) - SMT:
-%       xp=rand(1,n)>0.5;
-%       xs1=rand(1,n)>0.2;
-%       xs2=rand(1,n)>0.3;
-%
+      
 % power allocation for Primary User, User1 and User 2 - TNY:
 %       pp=0.5;
 %       p1=0.3; inverse from SMT: p1<->p2
@@ -23,7 +18,6 @@ close all; clear all;clc;
 % NOMA corrected: farther away from Base Station (BS), more allocated power;
 % Assuming the Base Station (BS) has total power of 1 and will allocate
 % power for each User as proportional to squared distance: Pwr ~ Dst^2
-%
         TotPwrBS_00 = 1.0;
           % previously suggested: 0.3
           PwrPUser_pp = TotPwrBS_00*(DstBStoPUser_dp^2)/SumSquareDst_00;
@@ -31,40 +25,23 @@ close all; clear all;clc;
           PwrUser1_p1 = TotPwrBS_00*(DstBStoUser1_ds1^2)/SumSquareDst_00;
           % previously suggested: 0.5
           PwrUser2_p2 = TotPwrBS_00*(DstBStoUser2_ds2^2)/SumSquareDst_00;
-%=============================================================================
-    %%% Create random binary messages/signals from Base Station (BS)
-%=============================================================================
-% signal of 'n' bits from BS to Primary User, User1 and User2 based on
-% with power allocation already applied??? - SMT:
-%       xp=rand(1,n)>0.5;
-%       xs1=rand(1,n)>0.2;
-%       xs2=rand(1,n)>0.3;
-% signal stream of Primary User, User1 and User2 - TNY:
-% 'rand' generates any number between [0 and 1], NOT binary, noise included???
-%       xp=rand(1,n);
-%       xs1=rand(1,n);
-%       xs2=rand(1,n);
-%
+
 % Correct (actual) binary messages of 'TxBits_n' bits length:
 % equal probability of 0 and 1 in every bit;
 %   'rand'  generates numbers in [0 to 1], uniformally distributed;
 %   Mean is 0.5
-%
         SgnPUser_xp = rand(1,TxBits_n) > 0.5;
         SgnUser1_xs1 = rand(1,TxBits_n) > 0.5;
         SgnUser2_xs2 = rand(1,TxBits_n) > 0.5;
-%=============================================================================
-    %%% Superposition Encoding
-%=============================================================================
+
+%%% Superposition Encoding
 % Direct sum of signals: incorrect  - SMT: X=xp+xs1+xs2;
 % NOMA: Power-domain Multiplexing, sum of products signal*sqrt(power) - TNY:
-%
         Enc_X = sqrt(PwrPUser_pp)*SgnPUser_xp;
         Enc_X = sqrt(PwrUser1_p1)*SgnUser1_xs1 + Enc_X;
         Enc_X = sqrt(PwrUser2_p2)*SgnUser2_xs2 + Enc_X;
-%=============================================================================
-    %%% Received signals for all Users
-%=============================================================================
+        
+%%% Received signals for all Users
 % Adding Gaussian Noise: use 'randn' instead of 'rand':
 %   'randn' generates numbers in [-Inf,+Inf], normally distributed (Gaussian);
 %   Mean is zero, but with strong concetration in [-1 to +1];
@@ -95,42 +72,20 @@ close all; clear all;clc;
   AtnGnPUser_00 = TotPwrBS_00/PwrPUser_pp * 1/SyLosPUser_00;
   AtnGnPUser_00 = AtnGnPUser_00*(DstBStoPUser_dp^2)/(MaxDsttoUser_00^2);
   AtnGnPUser_00 = 1 - AtnGnPUser_00;
-    SyLosUser1_00 = 1 + rand/(10*PwrUser1_p1);
-    AtnGnUser1_00 = TotPwrBS_00/PwrUser1_p1 * 1/SyLosUser1_00;
-    AtnGnUser1_00 = AtnGnUser1_00*(DstBStoUser1_ds1^2)/(MaxDsttoUser_00^2);
-    AtnGnUser1_00 = 1 - AtnGnUser1_00;
-    SyLosUser2_00 = 1 + rand/(10*PwrUser2_p2);
-    AtnGnUser2_00 = TotPwrBS_00/PwrUser2_p2 * 1/SyLosUser2_00;
-    AtnGnUser2_00 = AtnGnUser2_00*(DstBStoUser2_ds2^2)/(MaxDsttoUser_00^2);
-    AtnGnUser2_00 = 1 - AtnGnUser2_00;
-%=============================================================================
-    %%% Signal received by each User
-%=============================================================================
-% SMT: bit-lenght iteraction adding a Noise, increasing per bit - incorrect;
-% Adding signal/(squared distance) - incorrect;
-%        for s=0.1:0.01:1    % noise variation
-%            N=s*randn(1,n);
-%            ys1=xs1/ds1.^2+N+xp/dp.^2+xs2/ds2.^2
-%        end
-%
-% NOMA: the Superposition Encoding (Enc_X) containing the messages to all
-% Users already calculated above is affected by Attenuation (multiplier) and
-% Noise (additive) just one time only as in Linear equation below:
-%
-%          Yk(t) = X(t).Gk + Wk(t)     %where
-%
-% Yk(t) = superimposed signal received by User[k];
-% X(t)  = superimposed signal with all Users messages as transmitted by BS;
-% Gk    = channel attenuation gain for the link between BS and User[k];
-% Wk(t) = additive White Gaussian Noise (AWGN) at the User[k] with
-%         mean ZERO and density N0;
-% 
+  SyLosUser1_00 = 1 + rand/(10*PwrUser1_p1);
+  AtnGnUser1_00 = TotPwrBS_00/PwrUser1_p1 * 1/SyLosUser1_00;
+  AtnGnUser1_00 = AtnGnUser1_00*(DstBStoUser1_ds1^2)/(MaxDsttoUser_00^2);
+  AtnGnUser1_00 = 1 - AtnGnUser1_00;
+  SyLosUser2_00 = 1 + rand/(10*PwrUser2_p2);
+  AtnGnUser2_00 = TotPwrBS_00/PwrUser2_p2 * 1/SyLosUser2_00;
+  AtnGnUser2_00 = AtnGnUser2_00*(DstBStoUser2_ds2^2)/(MaxDsttoUser_00^2);
+  AtnGnUser2_00 = 1 - AtnGnUser2_00;
+
         RxSgnPUser_ysp = Enc_X*AtnGnPUser_00 + NoisePUser_N;
         RxSgnUser1_ys1 = Enc_X*AtnGnUser1_00 + NoiseUser1_N;
         RxSgnUser2_ys2 = Enc_X*AtnGnUser2_00 + NoiseUser2_N;
+        
        figure(1)
-%        subplot(3,1,1);plot(t_s,RxSgnPUser_ysp);
-%        xlabel('Time (s)');ylabel('Amplitude')
        subplot(2,1,1);plot(t_s,RxSgnUser1_ys1);
        title('User 1');xlabel('Time (s)');ylabel('Amplitude');grid on
        subplot(2,1,2);plot(t_s,RxSgnUser2_ys2);
